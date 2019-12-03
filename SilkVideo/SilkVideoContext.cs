fe53ec5a9b3b7f271094743abc52620c30eecdc8
András Rozner <roznernek@gmail.com>
2019-12-03 11:18:45 +0100

    Created Db base

---
+++ SilkVideo/SilkVideoContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SilkVideo.Models;
+
+namespace SilkVideo
+{
+    public class SilkVideoContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SilkVideo;Trusted_Connection=True;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Video>()
+                .Property(b => b.Id)
+                .IsRequired();
+
+            modelBuilder.Entity<Comment>()
+                .Property(b => b.Id)
+                .IsRequired();
+        }
+    }
+}
--- SilkVideo/Startup.cs
+++ SilkVideo/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace SilkVideo
 {
@@ -20,6 +21,8 @@ namespace SilkVideo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SilkVideoContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("SilkVideo")));
 
             services.AddControllersWithViews();
 
