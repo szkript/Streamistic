41fac70e5330ff0b46617a9ac9a8c7efa9be008a
Szanyi Krisztián <szkript@gmail.com>
2019-12-05 20:56:45 +0100

    refactor

--- SilkVideo/ClientApp/src/components/Login.js
+++ SilkVideo/ClientApp/src/components/Login.js
@@ -15,7 +15,7 @@ export class Login extends Component {
         // check it out: we get the evt.target.name (which will be either "email" or "password")
         // and use it to target the key on our `state` object with the same name, using bracket syntax
         this.setState({ [evt.target.name]: evt.target.value });
-        console.warn("userField: ", this.state.username, "pwField: ", this.state.password);
+        //console.warn("userField: ", this.state.username, "pwField: ", this.state.password);
     }
 
     handleClick = () => {
