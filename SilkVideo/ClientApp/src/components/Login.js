94dbe14f2a30cf8cd0374d088b9c61fb8a5d4f64
Szanyi Krisztián <szkript@gmail.com>
2019-12-05 13:50:03 +0100

    ERRRORR

--- SilkVideo/ClientApp/src/components/Login.js
+++ SilkVideo/ClientApp/src/components/Login.js
@@ -9,39 +9,48 @@ export class Login extends Component {
             password: ''
         };
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(evt) {
         // check it out: we get the evt.target.name (which will be either "email" or "password")
         // and use it to target the key on our `state` object with the same name, using bracket syntax
         this.setState({ [evt.target.name]: evt.target.value });
-        console.warn("userField: ",this.state.username, "pwField: ", this.state.password);
+        console.warn("userField: ", this.state.username, "pwField: ", this.state.password);
     }
 
-    handleSubmit(event) {
-        event.PreventDefault();
-        const url = "https://localhost:44321/api/Login";
+    handleSubmit(event, uname, pw) {
         //const uname = event.target.username;
         //const userData = {
         //    UserName: { uname},
         //    //Password: { }
         //}
-        console.warn(url);
+        console.warn("lefut?");
         //return post(url, datas)
         //    .then(response => console.warn("result", response))
+    }
+    handleClick = () => {
+        console.log('this is:', this);
+        const url = "https://localhost:44321/api/Login";
+        const uname = this.state.username;
+        const pw = this.state.password;
+        const loginData = {
+            Username: { uname },
+            Password: { pw }
+        };
+        return post(url, loginData)
+            .then(response => console.warn("result", response))
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form>
 
                 <label>Username</label>
                 <input type="text" name="username" onChange={this.handleChange} />
 
                 <label>Password</label>
                 <input type="password" name="password" onChange={this.handleChange} />
-                <input type="submit" value="submit" />
+                <input type="button" value="submit" onClick={this.handleClick} />
             </form>
         );
     }
