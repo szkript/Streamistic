bc94048f1de9f3747971d3de0216eed6674f3dd0
András Rozner <roznernek@gmail.com>
2019-12-19 10:14:52 +0100

    Streaming controller for livestream

---
+++ SilkVideo/Controllers/StreamingController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SilkVideo.Models;
+using System.Threading.Tasks;
+
+namespace SilkVideo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StreamingController : ControllerBase
+    {
+        private readonly SilkVideoContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public StreamingController(SilkVideoContext context, UserManager<User> usermanager)
+        {
+            _context = context;
+            _userManager = usermanager;
+        }
+
+        [HttpGet]
+        public async Task<Video> GetStreamById(long Id)
+        {
+            var video = _context.Videos.Find(Id);
+            return video;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StartLiveStream(Video stream)
+        {
+            var user = await _userManager.FindByNameAsync("ezaz");
+            Video liveStream = stream;
+            user.Videos.Add(liveStream);
+            _context.Update(user);
+            var updatedStream = await _context.Videos.FindAsync(stream);
+            return Redirect("localhost:/stream/"+ updatedStream.Id);
+        }
+
+
+    }
+}
