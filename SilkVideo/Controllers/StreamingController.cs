a064829e429620015a28f0ee83e6a0b7401040cc
Szanyi Krisztián <szkript@gmail.com>
2020-01-15 15:32:50 +0100

    basic experimenting

--- SilkVideo/Controllers/StreamingController.cs
+++ SilkVideo/Controllers/StreamingController.cs
@@ -14,6 +14,7 @@ namespace SilkVideo.Controllers
     [ApiController]
     public class StreamingController : ControllerBase
     {
+        RecordService.ServiceClient recordService = new RecordService.ServiceClient();
         private readonly SilkVideoContext _context;
         private readonly UserManager<User> _userManager;
 
@@ -30,7 +31,7 @@ namespace SilkVideo.Controllers
             return video;
         }
 
-        
+
         [HttpPost]
         [Route("record")]
         public async Task<IActionResult> StartLiveStreamRecord()
@@ -38,7 +39,8 @@ namespace SilkVideo.Controllers
             string username = this.User.Identity.Name;
             if (username != null)
             {
-                await StreamRecording(username);
+                await recordService.StreamRecordingAsync(username);
+                //await StreamRecording(username);
             }
 
             return Ok();
@@ -61,58 +63,59 @@ namespace SilkVideo.Controllers
             //return Redirect("localhost:/stream/"+ updatedStream.Id);
         }
 
-        private async Task StreamRecording(string username)
-        {
-            Video video = new Video();
-            video.Path = null;
-            Process rtmpdump = new Process();
-            rtmpdump.StartInfo.FileName = "cmd.exe";
-            rtmpdump.StartInfo.Arguments = "/C rtmpdump -q --rtmp rtmp://64.225.24.130:1935/show --playpath " + username + " -o Videos/"+username+".flv --live";
-            rtmpdump.StartInfo.UseShellExecute = true;
-            rtmpdump.StartInfo.RedirectStandardOutput = false;
-            await Task.Run(() => {
-                int counter = 0;
-                while (true)
-                {
-                    rtmpdump.Start();
-                    rtmpdump.WaitForExit();
-                    FileInfo file = new FileInfo("Videos/"+ username + ".flv");
-                    if (file.Length > 0)
-                    {
-                        DateTime uploadDate = DateTime.Now;
-                        string formattedUploadDate ="" + uploadDate.Year + uploadDate.Month + uploadDate.Day + uploadDate.Hour + uploadDate.Minute + uploadDate.Second;
-                        
-                        video.Description = "In Progress";
-                        video.Path = "Videos/" + username + formattedUploadDate + ".mp4";
-                        video.UploadTime = uploadDate;
-                        Process ffmpeg = new Process();
-                        ffmpeg.StartInfo.FileName = "cmd.exe";
-                        ffmpeg.StartInfo.Arguments = "/C ffmpeg -i Videos/"+username+".flv -c:v libx264 -crf 19 -strict experimental Videos/"+username+ formattedUploadDate +".mp4";
-                        ffmpeg.StartInfo.UseShellExecute = true;
-                        ffmpeg.StartInfo.RedirectStandardOutput = false;
-                        ffmpeg.Start();
-                        ffmpeg.WaitForExit();
-                        file.Delete();
-                        
-                        break;
-                    }
+        //private async Task StreamRecording(string username)
+        //{
+        //    Video video = new Video();
+        //    video.Path = null;
+        //    Process rtmpdump = new Process();
+        //    rtmpdump.StartInfo.FileName = "cmd.exe";
+        //    rtmpdump.StartInfo.Arguments = "/C rtmpdump -q --rtmp rtmp://64.225.24.130:1935/show --playpath " + username + " -o Videos/" + username + ".flv --live";
+        //    rtmpdump.StartInfo.UseShellExecute = true;
+        //    rtmpdump.StartInfo.RedirectStandardOutput = false;
+        //    await Task.Run(() =>
+        //    {
+        //        int counter = 0;
+        //        while (true)
+        //        {
+        //            rtmpdump.Start();
+        //            rtmpdump.WaitForExit();
+        //            FileInfo file = new FileInfo("Videos/" + username + ".flv");
+        //            if (file.Length > 0)
+        //            {
+        //                DateTime uploadDate = DateTime.Now;
+        //                string formattedUploadDate = "" + uploadDate.Year + uploadDate.Month + uploadDate.Day + uploadDate.Hour + uploadDate.Minute + uploadDate.Second;
 
-                    if (counter > 10)
-                    {
-                        file.Delete();
-                        break;
-                    }
+        //                video.Description = "In Progress";
+        //                video.Path = "Videos/" + username + formattedUploadDate + ".mp4";
+        //                video.UploadTime = uploadDate;
+        //                Process ffmpeg = new Process();
+        //                ffmpeg.StartInfo.FileName = "cmd.exe";
+        //                ffmpeg.StartInfo.Arguments = "/C ffmpeg -i Videos/" + username + ".flv -c:v libx264 -crf 19 -strict experimental Videos/" + username + formattedUploadDate + ".mp4";
+        //                ffmpeg.StartInfo.UseShellExecute = true;
+        //                ffmpeg.StartInfo.RedirectStandardOutput = false;
+        //                ffmpeg.Start();
+        //                ffmpeg.WaitForExit();
+        //                file.Delete();
 
-                    counter++;
-                }
-            });
+        //                break;
+        //            }
 
-            if (video.Path != null)
-            {
-                await SaveVideoAsync(username, video);
-            }
-            
-        }
+        //            if (counter > 10)
+        //            {
+        //                file.Delete();
+        //                break;
+        //            }
+
+        //            counter++;
+        //        }
+        //    });
+
+        //    if (video.Path != null)
+        //    {
+        //        await SaveVideoAsync(username, video);
+        //    }
+
+        //}
 
         private async Task SaveVideoAsync(string username, Video video)
         {
